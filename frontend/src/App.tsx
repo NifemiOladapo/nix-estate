d6ce47f63b4434c53c867ad844bee1dc4ea4fac3
Nifemi Oladapo <nifethedev@gmail.com>
2024-11-16 14:47:39 -0800

    added page routes using react-router-dom

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,21 @@
+import { Route, Routes } from "react-router-dom";
+
+import Profile from "./pages/Profile.js";
+import Signup from "./pages/SignUp.js";
+import SignIn from "./pages/SignIn.js";
+import Home from "./pages/Home.js";
+import About from "./pages/About.js";
+
 const App = () => {
   return (
-    <div>
-      <h1 className="text-red-200 text-5xl text-center">Real Estate Application</h1>
-    </div>
-  )
-}
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/sign-up" element={<Signup />} />
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/profile" element={<Profile />} />
+    </Routes>
+  );
+};
 
 export default App;
