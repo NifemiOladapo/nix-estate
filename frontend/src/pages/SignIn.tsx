6cccf4604565a36b16d95172b6458ac181c551c0
Nifemi Oladapo <nifethedev@gmail.com>
2025-01-02 02:02:58 -0800

    added frontend login form functionality

--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -1,7 +1,78 @@
-const SignIn = () => {
-  return (
-    <div>SignIn</div>
-  )
-}
+import { FormEvent, useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
-export default SignIn
+const SignIn = () => {
+  const [formData, setFormData] = useState({});
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const navigate = useNavigate();
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData((formData) => ({ ...formData, [e.target.id]: e.target.value }));
+  };
+
+  const handleSubmit = async (e: FormEvent) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      if (data.success === false) {
+        return setError(data.message);
+      }
+      console.log(data)
+      setError(null);
+      navigate("/");
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return (
+    <main className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
+      <form onSubmit={handleSubmit} className="flex flex-col gap-4">
+        <input
+          type="email"
+          id="email"
+          className="border p-3 rounded-lg"
+          required
+          placeholder="Input Your Email"
+          onChange={handleChange}
+        />
+        <input
+          type="password"
+          id="password"
+          className="border p-3 rounded-lg"
+          required
+          placeholder="Input Your Password"
+          onChange={handleChange}
+        />
+        <button
+          disabled={loading}
+          className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
+        >
+          {loading ? "Signing You In..." : "Sign In"}
+        </button>
+      </form>
+      <div className="flex gap-2 mt-5">
+        <p>Dont Have an account?</p>
+        <Link to={"/sign-up"}>
+          <span className="text-blue-700">Sign Up</span>
+        </Link>
+      </div>
+      <p className="text-red-600">{error}</p>
+    </main>
+  );
+};
+
+export default SignIn;
