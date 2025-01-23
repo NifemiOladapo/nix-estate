f7df2cbdaf697014ff355f4cd63454f3484eaf88
nifemi oladapo <nifethedev@gmail.com>
2025-01-23 12:07:27 -0800

    created backend func for google OAuth signin

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -27,12 +27,48 @@ export const signin = async (req, res, next) => {
     const validPassword = bcryptjs.compareSync(password, validUser.password);
     if (!validPassword) return next(errorHandler("Wrong Credentials", 401));
     const { password: pass, ...rest } = validUser._doc;
-    const token = jwt.sign({ id: "validUser._id " }, process.env.JWT__SECRET);
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT__SECRET);
     res
       .cookie("access__token", token, { httpOnly: true })
       .status(200)
       .json(rest);
     console.log("hi");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const google = async (req, res, next) => {
+  const { email, username, profilePicture } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (user) {
+      const token = jwt.sign({ id: user._id }, process.env.JWT__SECRET);
+      res
+        .cookie("access__token", token, { httpOnly: true })
+        .status(200)
+        .json(rest);
+    } else {
+      const generatedPassword =
+        Math.random().toString(36).slice(-8) +
+        Math.random().toString(36).slice(-8);
+      const hashedPassword = bcryptjs.hashSync(generatedPassword, 10);
+      const newUser = new User({
+        username:
+          username.split(" ").join("").toLowercase() +
+          Math.random().toString(36).slice(-4),
+        email,
+        password: hashedPassword,
+        profilePicture,
+      });
+      await newUser.save();
+      const token = jwt.sign({ id: newUser._id }, process.env.JWT__SECRET);
+      const { password: pass, ...rest } = newUser._doc;
+      res
+        .cookie("access__token", token, { httpOnly: true })
+        .status(200)
+        .json(rest);
+    }
   } catch (error) {
     next(error);
   }
