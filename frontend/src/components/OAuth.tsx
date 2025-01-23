3b4d25a1f79770ee5ec89831a058341e53d7928c
nifemi oladapo <nifethedev@gmail.com>
2025-01-23 11:41:55 -0800

    created OAuth button component,but the func is yet to be implemented

---
+++ frontend/src/components/OAuth.tsx
@@ -0,0 +1,14 @@
+const OAuth = () => {
+  const handleGoogleClick = () => {};
+  return (
+    <button
+      type="button"
+      onClick={handleGoogleClick}
+      className="bg-red-700 text-white p-3 rounded-lg uppercase hover:opacity-95"
+    >
+      Continue with Google
+    </button>
+  );
+};
+
+export default OAuth;
--- frontend/src/pages/SignIn.tsx
+++ frontend/src/pages/SignIn.tsx
@@ -6,6 +6,7 @@ import {
   signInFailure,
   signInStart,
 } from "../redux/user/userSlice.js";
+import OAuth from "../components/OAuth.js";
 
 const SignIn = () => {
   const [formData, setFormData] = useState({});
@@ -65,6 +66,7 @@ const SignIn = () => {
         >
           {loading ? "Signing You In..." : "Sign In"}
         </button>
+        <OAuth />
       </form>
       <div className="flex gap-2 mt-5">
         <p>Dont Have an account?</p>
